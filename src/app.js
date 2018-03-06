77c79af82090afb8641e6390cc7aaef51d2e88c7
natwipool <30383412+natwipool@users.noreply.github.com>
2018-03-06 21:18:10 +0700

    Setup production webpack built"

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { setTransPrayers } from './actions/transPrayers';
-import { addPlaylist } from './actions/playlists';
-import getPlaylistData from './utils/getPlaylistData';
 import transPrayersData from './data/transPrayersData';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -13,19 +11,6 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(setTransPrayers(transPrayersData));
-
-store.dispatch(addPlaylist(
-  { description: 'My first playlist', lists: ['อภิณหปัจจเวกขณปาฐะ', 'บทพิจารณาสังขาร'] })
-)
-
-const playlistTwo = store.dispatch(addPlaylist(
-  { description: 'My secound playlist', lists: ['สัพพปัตติทานคาถา', 'บทแผ่เมตตา'] })
-)
-
-const state = store.getState();
-const getFullPlaylist = getPlaylistData(state.transPrayers, playlistTwo.playlist.lists);
-console.log(state)
-console.log(getFullPlaylist);
 
 const jsx = (
   <Provider store={store}>
